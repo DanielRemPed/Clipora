8325d09e890d124674e83132ca3d4826deab19c5
DanielRemPed <daniel.remped@gmail.com>
2026-07-12 22:40:53 -0400

    Attempting to fix to go back to index

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route("/")
-
-def home():
+def index():
     return render_template("index.html")
 
 @app.route('/login')
